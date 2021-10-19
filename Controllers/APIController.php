15679e29c22f5b81d07a4a93adf6291cfbbdb264
carovi0696 <yabsurdo2op>
2021-10-19 18:29:04 +0200

    First Init

---
+++ Controllers/APIController.php
@@ -0,0 +1,22 @@
+<?php
+namespace controllers
+
+class APIController
+{
+    //Otra manera que se me ocurre para guardar las url de las apis y que sea mas accessible seria mediante un fichero de configuración
+
+    const AUTH_PATH = '/auth/login';
+    const PUT_CLIENTS_PATH = '/client/put';
+    const POST_CLIENTS_PATH = '/client/post';
+    const GET_CLIENTS_PATH = '/client/get';
+    const GET_ONE_CLIENT_PATH = '/client/get/one/';
+    const GET_CLIENTS_SEARCH_PATH = '/client/get/search';
+    const DELETE_CLIENT_PATH = '/client/delete/';
+
+
+    
+    public function getAuth(){
+
+        
+    }
+}
---
+++ Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+namespace controllers
+
+class UserController
+{
+    public $public = 'Public';
+    protected $protected = 'Protected';
+    private $private = 'Private';
+
+    function printHello()
+    {
+        echo $this->public;
+        echo $this->protected;
+        echo $this->private;
+    }
+}
